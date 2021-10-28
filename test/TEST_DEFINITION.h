6287fa8eb228f3e999fe11ccbac8aa45e11b5b1e
Bunyamin TAMAR <bunyamin.tamar@asisct.com>
2021-10-28 13:47:56 +0300

    add empty line for end of file

--- test/TEST_DEFINITION.h
+++ test/TEST_DEFINITION.h
@@ -1,3 +1,6 @@
+#ifndef TEST_DEFINITON_H
+#define TEST_DEFINITON_H
+
 #include <gtest/gtest.h>
 
 #define TEST_RUN()                        \
@@ -6,3 +9,5 @@ int main(int argc, char **argv)           \
     testing::InitGoogleTest(&argc, argv); \
     return RUN_ALL_TESTS();               \
 }
+
+#endif
