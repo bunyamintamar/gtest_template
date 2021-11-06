0542f4ac9c631801f4734b7a77bfe4f550124372
Bunyamin TAMAR <bunyamintamar@yandex.com>
2021-11-07 01:55:59 +0300

    correct programmer info part at installer script

--- install_googletest.sh
+++ install_googletest.sh
@@ -62,11 +62,11 @@ echo ----------------------------------------------------
 echo
 echo
 echo
-echo '-----------------------------------------------------------------'
-echo '|  written by BUNYAMIN TAMAR                                    |'
-echo '|  Mail     -> bunyamintamar@yandex.com                         |'
-echo '|  GitHub   -> https://github.com/bunyamintamar/gtest_template  |'
-echo '|  Linkedin -> https://www.linkedin.com/in/bunyamintamar        |'
-echo '-----------------------------------------------------------------'
+echo '----------------------------------------------------------------'
+echo '|  written by BUNYAMIN TAMAR                                   |'
+echo '|  Mail     : bunyamintamar@yandex.com                         |'
+echo '|  GitHub   : https://github.com/bunyamintamar/gtest_template  |'
+echo '|  Linkedin : https://www.linkedin.com/in/bunyamintamar        |'
+echo '----------------------------------------------------------------'
 echo good luck!
 echo
